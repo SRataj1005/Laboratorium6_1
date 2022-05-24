2f293305dc2c58cb6d7a179655ee1e0f04863a05
Sebastian Rataj <100698720+SRataj1005@users.noreply.github.com>
2022-05-24 13:26:02 +0000

    Commit2 (nie działa)

--- script.js
+++ script.js
@@ -4,5 +4,15 @@
  {
    ex1_content.innerHTML = "0,1,2,3,4,5,6,7,8,9";
  })
+   const pn = ex2_text.querySelector("Phone number");
+   ex2_text.addEventListener("input", function(){
+     if(ex2_text.lenght=9)
+     {
+       ex2_content.innerHTML = "Dobry numer";
+     }
+     else
+     {
+       ex2_content.innerHTML = "Zły numer";
+     }
 }
 )();
